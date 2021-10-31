2572da9166ea9bef04a7ebceba16de9d50aa5df7
Vanessa Taylor <vanessa@sparkfund.co>
2021-10-31 16:32:57 -0500

    nonconcurrent code

--- parellelLetterFrequency/parellel/lib/parellel.ex
+++ parellelLetterFrequency/parellel/lib/parellel.ex
@@ -17,20 +17,26 @@ defmodule Parellel do
     texts
     # take all the strings and joins them together into one string
     |> Enum.join()
-    # then new string is passed to next function
+    # then the new string is passed to next function
     # returns a list of all the individual characters in the string
     |> String.elements()
   end
 
   defp count_letters(elements) do
+    # reduce will return acc, passing in emunerable, empty map, function with the individual element and acc
     Enum.reduce(elements, %{}, fn element, acc ->
-      nil
-      # if element is a letter
-      # downcase the letter
-      # increment the count of letter in acc
-
-      # else
-      # do nothing return acc
+      # if element is a letter, match takes a string and expression. 
+      # regular expression, front and back of the string match, a single char in any language, unicode
+      if String.match?(element, ~r/^\p{L}$/u) do
+        # downcase the letter
+        downcased_letter = String.downcase(element)
+        # increment the count of letter in acc
+        # pass acc, key that is going to be updated, if its not on the map set value to 1, if there is already a count run fn.
+        Map.update(acc, downcased_letter, 1, fn count -> count + 1 end)
+      else
+        # do nothing return acc
+        acc
+      end
     end)
   end
 end
